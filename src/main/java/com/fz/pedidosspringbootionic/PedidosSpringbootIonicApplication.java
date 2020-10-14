cd679cf039ac39403b7b67d1eab6fe8232e6960b
zambrinf <zambrinf@gmail.com>
2020-10-14 20:33:27 -0300

    Project created

---
+++ src/main/java/com/fz/pedidosspringbootionic/PedidosSpringbootIonicApplication.java
@@ -0,0 +1,13 @@
+package com.fz.pedidosspringbootionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosSpringbootIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosSpringbootIonicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fz/pedidosspringbootionic/PedidosSpringbootIonicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fz.pedidosspringbootionic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosSpringbootIonicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
