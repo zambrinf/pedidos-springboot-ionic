8e4630254de989eaa9ced312888ace83e85043fa
Felipe Zambrin <zambrinf@gmail.com>
2020-10-15 09:13:03 -0300

    Teste classe de dominio Categoria

--- src/main/java/com/fz/pedidosspringbootionic/resources/CategoriaResource.java
+++ src/main/java/com/fz/pedidosspringbootionic/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package com.fz.pedidosspringbootionic.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fz.pedidosspringbootionic.domain.Categoria;
+
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+
 	@GetMapping
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática"); //mock
+		Categoria cat2 = new Categoria(2, "Escritório"); //mock
+
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+
+		return lista;
 	}
 }
