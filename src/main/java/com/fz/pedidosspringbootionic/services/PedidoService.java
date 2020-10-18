108edfbfe16d0bd63ecb4378d15d94eb9ca5fc00
zambrinf <zambrinf@gmail.com>
2020-10-18 13:03:38 -0300

    Endpoint /pedidos/{id}

---
+++ src/main/java/com/fz/pedidosspringbootionic/services/PedidoService.java
@@ -0,0 +1,30 @@
+package com.fz.pedidosspringbootionic.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fz.pedidosspringbootionic.domain.Pedido;
+import com.fz.pedidosspringbootionic.repositories.PedidoRepository;
+import com.fz.pedidosspringbootionic.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repository;
+	
+	public Pedido findById(Integer id) {
+		Optional<Pedido> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		 "Objeto não encontrado! Id: " + id)); 
+	}
+	
+	public List<Pedido> findAll() {
+		return repository.findAll();
+	}
+	
+	
+}
