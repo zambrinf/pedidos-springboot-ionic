c0673654923c9b5367500c9ff2f5aeeb92bca500
zambrinf <zambrinf@gmail.com>
2020-10-18 12:47:19 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/fz/pedidosspringbootionic/PedidosSpringbootIonicApplication.java
+++ src/main/java/com/fz/pedidosspringbootionic/PedidosSpringbootIonicApplication.java
@@ -13,6 +13,7 @@ import com.fz.pedidosspringbootionic.domain.Cidade;
 import com.fz.pedidosspringbootionic.domain.Cliente;
 import com.fz.pedidosspringbootionic.domain.Endereco;
 import com.fz.pedidosspringbootionic.domain.Estado;
+import com.fz.pedidosspringbootionic.domain.ItemPedido;
 import com.fz.pedidosspringbootionic.domain.Pagamento;
 import com.fz.pedidosspringbootionic.domain.PagamentoComBoleto;
 import com.fz.pedidosspringbootionic.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.fz.pedidosspringbootionic.repositories.CidadeRepository;
 import com.fz.pedidosspringbootionic.repositories.ClienteRepository;
 import com.fz.pedidosspringbootionic.repositories.EnderecoRepository;
 import com.fz.pedidosspringbootionic.repositories.EstadoRepository;
+import com.fz.pedidosspringbootionic.repositories.ItemPedidoRepository;
 import com.fz.pedidosspringbootionic.repositories.PagamentoRepository;
 import com.fz.pedidosspringbootionic.repositories.PedidoRepository;
 import com.fz.pedidosspringbootionic.repositories.ProdutoRepository;
@@ -54,6 +56,8 @@ public class PedidosSpringbootIonicApplication implements CommandLineRunner {
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	//Command Line Runner - na iniciação - Mock
 	@Override
@@ -120,6 +124,19 @@ public class PedidosSpringbootIonicApplication implements CommandLineRunner {
 		
 		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
 		
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1,ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1,ip2,ip3));
 		
 	}
 
